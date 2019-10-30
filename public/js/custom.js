0798ee6bef1e069178294593b0fc1636250e76d5
Customer <u224-9g6vuubnud8y@sgp1012.siteground.asia>
2019-10-30 14:13:30 +0000

    first section 50%

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-    const faceButton = document.querySelector('.face-button');
-    // const tween 
+    // const faceButton = document.querySelector('.face-button');
+    
+    // Set stroke dasharray of logo text after delay
+    const mainText = document.querySelectorAll('.logo-text .main-text');
+    
+    mainText.forEach(function(item){
+        setTimeout(function() {
+            item.style.setProperty('stroke-dasharray', 200, 'important')
+            item.style.setProperty('stroke-dashoffset', 0, 'important')
+        }, 1000);
+    });
+    
+    // Set random time for lightbulb to flicker
+    const flicker = document.querySelectorAll('.flicker');
+
+    function changeAnimationTime() {
+        var animationDuration = Math.random();
+        console.log(animationDuration)
+        flicker.forEach(function(item){
+            item.style.setProperty('--animation-time', animationDuration +'s');
+        });
+    }
+
+    setInterval(changeAnimationTime, 1000);
+
 });
